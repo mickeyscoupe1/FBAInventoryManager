efb3c030a0360fd19a13ee22d5d39e4b0c6d9d11
Fırat Topcu <frttpcdev@gmail.com>
2024-02-26 18:32:50 +0300

    Continue

--- Client/src/Components/Navbar.js
+++ Client/src/Components/Navbar.js
@@ -4,7 +4,9 @@ function Navbar() {
   return (
     <div className="navbar'">
         <div className="navbar-container">
-
+          <Link to="/" className="navbar-logo">
+            
+          </Link>
         </div>
     </div>
   )
